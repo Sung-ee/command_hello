2e1ec418f6daf13945e97436a65e7592b44b4c7f
Sung-ee <48739889+Sung-ee@users.noreply.github.com>
2019-03-20 14:01:04 +0900

    Update hello.py

--- git_tutorial/hello.py
+++ git_tutorial/hello.py
@@ -1,7 +1,9 @@
 //for command line git tutorial
 //local repository modification
 //github modification
+
 print("hello wolrd")
 print("tell your world")
 print("tell his world")
 print("tell her world")
+print("tell my world")
